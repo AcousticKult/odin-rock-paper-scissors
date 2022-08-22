c2274510aada4e81a593d9749388774f154d3327
AcousticKult <acoustickult@gmail.com>
2022-08-22 19:48:43 -0400

    Edit display functions

--- game.js
+++ game.js
@@ -1,54 +1,53 @@
+// Randomly return rock || paper || scissors
 const computerArray = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    let randomChoice = computerArray[Math.floor(Math.random() * computerArray.length)];
+    return randomChoice;
+}
 
 // Create counters for player and computer wins
 let playerScore = 0;
 let computerScore = 0;
 
-// Randomly return rock || paper || scissors
-function getComputerChoice() {
-    let randomChoice = computerArray[Math.floor(Math.random() * computerArray.length)];
-    return randomChoice;
-    }
+function displayWinner() {
+    ++playerScore;
+    return `You Win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}.`;
+}
 
-function playRound(playerSelection, computerSelection) {
-    //  FIND A PATTERN
-    
-    let winnerText = `You Win! ${playerSelection} beats ${computerSelection}.`;
-    let loserText = `You Lose! ${computerSelection} beats ${playerSelection}.`;
-    let invalidText = 'No winner!';
-
-    if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        ++playerScore;
-        return winnerText;
-    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        ++computerScore;
-        return loserText;
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        ++playerScore;
-        return winnerText;
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        ++computerScore;
-        return loserText;
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        ++playerScore;
-        return winnerText;
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        ++computerScore;
-        return loserText;
-    } else {return invalidText}
-    //  RETURN strings based on comparison (might need to change RETURN value)
+function displayLoser() {
+    ++computerScore;
+    return `You Lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}.`;
 }
 
 let playerSelection = (prompt('Choose your weapon: rock 🪨, paper 🧻, scissors ✂')).toLowerCase();
 let computerSelection = getComputerChoice();
 
+// Play a round using playerSelection and computerSelection
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return displayWinner();
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return displayLoser();
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return displayWinner();
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return displayLoser();
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return displayWinner();
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return displayLoser();
+    } else {return 'No Winner!'}
+}
+
+// Play 5 rounds of the game
 function game() {
     for (let i = 0; i < 5; i++) {
         playRound();
         
         //  CONSOLE.LOG results of playRound()
         console.log(playRound(playerSelection, computerSelection));
-        console.log(`Player: ${playerSelection}, Score: ${playerScore} \nComputer: ${computerSelection}, Score: ${computerScore}`);
+        console.log(`Player: ${playerSelection.toUpperCase()}, Score: ${playerScore} \nComputer: ${computerSelection.toUpperCase()}, Score: ${computerScore}`);
 
         if (i < 4) {
             playerSelection = (prompt('Choose your weapon: rock 🪨, paper 🧻, scissors ✂')).toLowerCase();
@@ -56,7 +55,7 @@ function game() {
         } else {
             if (playerScore > computerScore) {
                 console.log("Congratulations! You are the winner! 🏆");
-            } else if (computerScore < playerScore) {
+            } else if (computerScore > playerScore) {
                 console.log("You lose! Better luck next time! 😔");
             } else {
                 console.log("There are no winners, only survivors 💀");
